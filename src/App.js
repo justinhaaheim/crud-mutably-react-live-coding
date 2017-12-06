99dca2c25730b9331fec97f108550aac15677a7d
Justin H <io@justinh.org>
2017-12-06 12:08:33 -0800

    Made EditableRow interactive

--- src/App.js
+++ src/App.js
@@ -1,19 +1,87 @@
 import React, { Component } from 'react';
 
-/* eslint-disable react/no-multi-comp, react/prefer-stateless-function */
+/* eslint-disable react/no-multi-comp */
+
+// What state does my app need/have?
+// When does it change?
 
 class EditableRow extends Component {
-  // TODO:
-  // Implement a row with state for whether the row is in editable mode or not.
-  // If it isn't editable, render the "edit" button and the title
-  // If it IS editable, render the "save" button and the editable text field
-  //
-  // A hint: think about what functions/event handlers should be part of this
-  // component and which functions should be _passed_ to this function as a
-  // "prop". A common pattern in react is to keep all the ajax logic in one
-  // place (App, in our case).
-  //
-  // Also, you'll want to pull this component out into its own .js file!
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      editable: false,
+      title: this.props.title,
+      author: this.props.author,
+    }
+
+    this.toggleEditable = this.toggleEditable.bind(this)
+    this.handleInput = this.handleInput.bind(this)
+  }
+
+  toggleEditable() {
+    const { bookId } = this.props
+
+    if (this.state.editable) {
+      this.setState({ editable: false })
+
+      // TODO:
+      // It's considered a React best practice to have your components be either
+      // *presentational* components (whose sole job is to present something)
+      // and *container* components (whose job is to handle the
+      // logic/fetching/etc and pass the right data to the right components).
+      // Given that, it'd be a good idea to refactor this code so that the PUT
+      // request that's happening here actually is written in the <App />
+      // component.
+      //
+      // How do we do that? It's common to pass *handler* functions down to
+      // presentational components from container components through props!
+      fetch(`http://mutably.herokuapp.com/books/${bookId}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+          title: this.state.title,
+          author: this.state.author,
+          image: this.props.imageUrl
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      })
+        .then(console.log)
+
+      return
+    }
+    this.setState({ editable: true })
+  }
+
+  handleInput(e) {
+    const name = e.target.name
+    console.log(name)
+    this.setState({
+      [name]: e.target.value
+    })
+  }
+
+  render() {
+    const { imageUrl } = this.props
+    const { editable, title, author } = this.state
+
+    return (
+      <div>
+        { editable ? (
+          <div>
+            <button className="btn btn-primary" onClick={this.toggleEditable}>Save</button>
+            <input value={title} className="form-control" name="title" onChange={this.handleInput} />
+            <input value={author} className="form-control" name="author" onChange={this.handleInput} />
+          </div>
+        ) : (
+          <div>
+            <button className="btn btn-success" onClick={this.toggleEditable}>Edit</button>
+            <span>{title}&nbsp;({author})</span>
+          </div>
+        )}
+        <img src={imageUrl} style={{ width: "50px", height: "50px" }} />
+      </div>
+    )
+  }
 }
 
 class App extends Component {
@@ -66,18 +134,21 @@ class App extends Component {
           <ul className="list-group">
             {
               books.map(book => (
-                <li>
-                  {book.title}&nbsp;
-                  ({book.author})
-                  <img src={book.image} style={{ width: "50px", height: "50px" }} />
+                <li className="list-group-item" key={book._id}>
+                  <EditableRow
+                    title={book.title}
+                    author={book.author}
+                    imageUrl={book.image}
+                    bookId={book._id}
+                  />
                 </li>
               ))
             }
           </ul>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
